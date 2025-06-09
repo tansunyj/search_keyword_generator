e6170bd249728497f5fd076a3ff3a696fdb5e4c6
tansunyj <jiejie0406@sohu.com>
2025-06-09 15:27:04 +0800

    添加文件

---
+++ vercel.config.js
@@ -0,0 +1,7 @@
+// This file helps Vercel identify this as a React project
+module.exports = {
+  projectType: 'react',
+  buildCommand: 'npm run build',
+  outputDirectory: 'build',
+  framework: 'create-react-app'
+}; 
